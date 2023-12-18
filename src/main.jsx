c6b8b68c3399cdf912476cb8e9e1af103a2050ce
unknown <shahariarmohammadhassan@gmail.com>
2023-12-18 23:14:13 +0600

    Helmet Provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router/Router.jsx'
+import { HelmetProvider } from 'react-helmet-async'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <RouterProvider router={router} />
+    <HelmetProvider>
+        <RouterProvider router={router} />
+    </HelmetProvider>
   
 )
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async"
 import Intro from "../../components/Intro"
 import Categories from "./Categories"
 import CustomersReview from "./CustomarsReview"
@@ -8,6 +9,9 @@ import SpecialDishes from "./SpecialDishes"
 const Home = () => {
   return (
     <div >
+      <Helmet>
+      <title>Foodie | Home</title>
+      </Helmet>
         <div className="mx-auto container px-6">
         <Intro/>
         <Categories/>
