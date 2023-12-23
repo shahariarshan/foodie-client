220868ab0106fb837a9ce3a352c28318cca27ed8
unknown <shahariarmohammadhassan@gmail.com>
2023-12-24 00:44:21 +0600

    items are on ui basted on category by tab based

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Card from '../home/card'
 
 const Menu = () => {
     const [menu, setMenu] = useState([])
@@ -12,7 +13,7 @@ const Menu = () => {
             try {
                 const response = await fetch('../../../public/menu.json')
                 const data = await response.json()
-                console.log(data);
+                // console.log(data);
                 setMenu(data)
                 setFilteredItem(data)
             }
@@ -21,17 +22,78 @@ const Menu = () => {
 
             }
         }
+        // call the function   
         fetchData()
     }, [])
+    // filtering data based on category 
+    const filterItems = (category) => {
+        const filtered = category === 'all' ? menu : menu.filter((item) => item.category === category)
+        setFilteredItem(filtered)
+        setSelectCategory(category)
+    }
+    // show all data function 
+    const showAll =()=>{
+        setFilteredItem(menu)
+        setSelectCategory('all')
+    }
+    // sorting base A-Z,a-z,H-L,L-H
+    const handelSortChange = (options)=>{
+        setSortOptions(options)
+        let sortItem =[...filteredItem];
+        switch(options) {
+
+            case "A-Z":
+            //   for assending order 
+            sortItem.sort((a,b)=>a.name.localCompare(b.name))
+              break;
+            case "Z-A":
+                sortItem.sort((a,b)=>b.name.localCompare(a.name))
+              break;
+            
+              
+            case "high to low":
+                sortItem.sort((a,b)=>b.price-a.price)
+              break;
+            case " low to high ":
+                sortItem.sort((a,b)=>a.price-b.price)
+              break;
+              default:
+                break;     
+          }
+          setFilteredItem(sortItem)
+
+    }
+
+
     return (
         <div className="mt-28 ">
+            {/* menu banner section  */}
             <div className="text-center items-center mx-auto space-y-4 px-7">
                 <h2 className="text-4xl font-bold">Dive into Delights Of Delectable <span className="text-green">Food</span></h2>
                 <p>Where Each Plate Weaves a Story of Culinary Mastery and Passionate Craftsmanship</p>
                 <button className="bg-green mx-auto item-center  text-white rounded-full px-6 btn">Order Now</button>
             </div>
+            {/* menu shop section  */}
             <div>
-
+            {/* >Filtering and sorting */}
+            {/* All category button  */}
+                <div className="flex  flex-row justify-center flex-wrap items-center md:gap-6 gap-4 mt-14 mb-5">
+                    
+                    <button onClick={showAll}>All</button>
+                    <button onClick={()=>filterItems('salad')}>Salad</button>
+                    <button onClick={()=>filterItems('pizza')}>Pizza</button>
+                    <button onClick={()=>filterItems('soup')}>Soups</button>
+                    <button  onClick={()=>filterItems('dessert')}>Desserts</button>
+                    <button  onClick={()=>filterItems('drinks')}>Drinks</button>
+                </div>
+                {/* all carts  */}
+                <div className="grid grid-cols-1 md:grid-cols-4 gap-5 mx-auto container">
+                    {
+                        filteredItem.map((item)=>(
+                            <Card key={item._id} item={item}/>
+                        ))
+                    }
+                </div>
 
             </div>
         </div>
--- src/pages/home/card.jsx
+++ src/pages/home/card.jsx
@@ -17,7 +17,7 @@ const Card = ({ item }) => {
 
         <div className="card card-compact  ml-5 rounded-2xl shadow-2xl relative ">
             <div className={`heartStar bg-green  absolute rating gap-1 top-2 right-2 ${isHeart ? "text-rose-500" : "text-white"}`}
-            onClick={handelHeart} >
+                onClick={handelHeart} >
 
                 <FaHeart className="h-10 w-10 p-2 cursor-pointer "></FaHeart>
             </div>
@@ -25,14 +25,20 @@ const Card = ({ item }) => {
                 <figure><img className="lg:px-10 hover:scale-125 duration-200 transition-all " src={image} alt="food" /></figure>
             </Link>
             <div className="card-body">
-               
-                <Link to={`/menu/${_id}`}>
-                <h2 className="card-title">{name}</h2>
-            </Link>
-                <p>{recipe}</p>
-                <p className="font-bold text-lg"><span className="text-xs">$</span>{price}</p>
 
+                <Link to={`/menu/${_id}`}>
+                    <h2 className="card-title">{name}</h2>
+                </Link>
+                <p>{recipe}</p>
+                <div className="flex justify-around ">
+                    <p className="font-bold text-lg"><span className="text-xs">$</span>{price}</p>
+                    <button className="btn bg-green flex gap-2 text-white rounded-full px-3 ">Add cart<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                    </svg>
+                    </button>
+                </div>
             </div>
+
         </div>
 
     );
